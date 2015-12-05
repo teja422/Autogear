6af7184a59f03426c75471246eb4fa4369ef47d0
RAVI KUMAR <ravikumar533@gmail.com>
2015-12-05 14:24:16 +0530

    autogear modified script

--- AutogearWeb/Repositories/IInstructor.cs
+++ AutogearWeb/Repositories/IInstructor.cs
@@ -1,12 +1,14 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 using AutogearWeb.EFModels;
 
 namespace AutogearWeb.Repositories
 {
-    public interface ILogin : IDisposable
+    public interface IInstructor : IDisposable
     {
         autogearEntities DataContext { get; set; }
 
+        
 
     }
 }
